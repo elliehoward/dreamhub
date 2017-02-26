63183d5e1650b63548413cc66cc304f6a313f7dc
Eleanor Howard <ellie@lizthedeveloper.com>
2017-02-25 23:21:03 -0800

    react-create-app on Heroku

---
+++ dreamhub/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>&#9729; Welcome to Dreamhub </h2>
+          <h3>"What a strange dream..."</h3>
+        </div>
+        <p className="App-intro">
+
+          Keep track of all your strange dreams here, read the dreams of others, and keep the conversation going in waking life.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
